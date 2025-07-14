fa80fe8466dba465b77d43a96646d771f2021334
gunjan280 <dogsgamers12345@gmail.com>
2025-07-14 23:58:33 +0530

    Initial commit

---
+++ Freelance-bidding-platform/client/src/pages/Login.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await axios.post('http://localhost:5000/api/auth/login', { username, password });
+        console.log(response.data);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default Login;
